c6ffa6af72f569275633bdb39cfb80f9fa819b21
Rafael Jardim <rafaelmjardim@gmail.com>
2024-02-16 23:18:56 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, SidebarComponent],
+  imports: [RouterOutlet, MainComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
