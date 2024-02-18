26a06e33db5ac5f58697b3dbc2e1a132b1868614
Rafael Jardim <rafaelmjardim@gmail.com>
2024-02-18 10:31:37 -0300

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionSearch } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ionSearch})],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionCreateOutline } from "@ng-icons/ionicons";
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    NgIcon, 
+  ],
+  providers: [provideIcons({ionCreateOutline })],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
