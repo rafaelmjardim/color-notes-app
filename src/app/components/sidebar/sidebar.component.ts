a2f5679028296c5fd96b733a30efe7aa570a8e86
Rafael Jardim <rafaelmjardim@gmail.com>
2024-02-18 10:34:10 -0300

    ajustado btn sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionAddOutline } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIcon
+  ],
+  providers: [provideIcons({ionAddOutline})],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
