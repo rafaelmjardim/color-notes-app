4f19fce7ecfa17762084ff743c3910b288e165bc
Rafael Jardim <rafaelmjardim@gmail.com>
2024-02-20 08:21:22 -0300

    update create dialog

--- src/app/components/create-note/create-note.component.ts
+++ src/app/components/create-note/create-note.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
-import {MatInput, MatInputModule} from '@angular/material/input';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MainService } from '../../pages/main/main.service';
+import { NoteType } from '../../pages/main/main';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-note',
   standalone: true,
-  imports: [
-    MatInputModule
+  imports: [ 
+    ReactiveFormsModule,
   ],
   templateUrl: './create-note.component.html',
   styleUrl: './create-note.component.scss'
 })
-export class CreateNoteComponent {
+export class CreateNoteComponent implements OnInit {
 
+  formNote!: FormGroup;
+
+  currentDate!: any;
+
+  constructor (
+    private main_service: MainService,
+    private dialog: MatDialog
+  ){}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm = () => {
+    this.formNote = new FormGroup({
+      textArea: new FormControl(''),
+      colorInput: new FormControl('')
+    })
+  }
+
+  submitCreateNote = () => {
+    const date: Date = new Date();
+    const dateFormated = date.toLocaleDateString('pt-BR');
+    
+    const textAreaValue = this.formNote.controls['textArea'].value;
+    const colorInput = this.formNote.controls['colorInput'].value;    
+
+    const newNote: NoteType = {
+      txt: textAreaValue,
+      date: dateFormated,
+      color: colorInput
+    }
+
+    this.main_service.postNotes(newNote).subscribe({
+      next: (notesResponse) => {
+        this.dialog.closeAll();
+      }
+    })
+  }
 }
